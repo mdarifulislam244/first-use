a39718e76720bd7fc786a2c8d62d978645391ce8
mdariful244 <mdariful2441139@gmail.com>
2023-11-16 23:46:29 +0600

    Add a new methode called greetings to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class Hello{
     public static void main(string[] args){
         system.out.println("Vs code is awesome..")
+        greetings("Merajul Islam");
+    }
+
+    public static void greetings(string name){
+        system.out.println("Welcome" + name);
     }
 }
